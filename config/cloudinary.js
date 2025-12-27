5f90643e900e78918ee493a554315522ca8989e1
sidharth <sidharth.p@blunetworld.com>
2025-12-27 15:14:22 +0530

    Fix Cloudinary API key env typo

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,7 +4,7 @@ const cloudinary=require('cloudinary').v2
 const connectCloudinary=async()=>{
     cloudinary.config({
         cloud_name:process.env.CLOUDINARYNAME,
-        api_key:process.env.CLODINARYAPIKEY,
+        api_key:process.env.CLOUDINARYAPIKEY,
         api_secret:process.env.CLOUDINARYSECRETKEY
     })
 
